14444464a3fce4dd88b8fdc91d6306fa8e991ccb
Leite <paulo.leite@capco.com>
2022-11-23 11:03:00 -0300

    feat: migrate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { NgChartsModule } from "ng2-charts";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app.routing.module";
@@ -13,7 +12,6 @@ import { AuthGuard } from "./shared/guards/auth.guard";
   imports: [
     BrowserModule,
     NgbModule,
-    NgChartsModule,
     AppRoutingModule,
     HttpClientModule,
   ],
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { ChartConfiguration, ChartData, ChartEvent, ChartType } from "chart.js";
-import { BaseChartDirective } from "ng2-charts";
+import { Component, OnInit } from "@angular/core";
 import { StandingsStateService } from "src/app/shared/state/standings/standings-state.service";
 import { Group } from "../standing/models/standing-response.model";
 import { StandingService } from "../standing/standing.service";
@@ -11,10 +9,6 @@ import { StandingService } from "../standing/standing.service";
   styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
-  @ViewChild(BaseChartDirective, { static: false }) chart:
-    | BaseChartDirective
-    | undefined;
-
   groups: Group[];
   totalGoals = 0;
   totalMatches = 0;
@@ -47,50 +41,5 @@ export class DashboardComponent implements OnInit {
         this.totalPoints += Number(team.pts);
       });
     });
-  }
-
-  public barChartOptions: ChartConfiguration["options"] = {
-    responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10,
-      },
-    },
-    plugins: {
-      legend: {
-        display: true,
-      },
-    },
-  };
-  public barChartType: ChartType = "bar";
-  public barChartData: ChartData<"bar"> = {
-    labels: ["2006", "2007", "2008", "2009", "2010", "2011", "2012"],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: "Series A" },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: "Series B" },
-    ],
-  };
-
-  // events
-  public chartClicked({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: {}[];
-  }): void {
-    console.log(event, active);
-  }
-
-  public chartHovered({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: {}[];
-  }): void {
-    console.log(event, active);
   }
 }
